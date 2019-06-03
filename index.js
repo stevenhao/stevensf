f2bf4c7a29d669e559b751b422e38e84bfae0870
stevenhao <stevenhao@gmail.com>
2019-06-02 18:49:46 -0700

    no cache

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 async function load() {
-  const response = await (await fetch('/response')).text();
+  const response = await (await fetch('/response', { cache: 'no-cache' })).text();
   const responseDiv = document.createElement('div');
   responseDiv.innerHTML = response;
   document.body.appendChild(responseDiv);
-  const date = await (await fetch('/date')).text();
+  const date = await (await fetch('/date', { cache: 'no-cache' })).text();
   const dateDiv = document.createElement('div');
   dateDiv.innerHTML = `Last updated: ${date}`;
   document.body.appendChild(dateDiv);
